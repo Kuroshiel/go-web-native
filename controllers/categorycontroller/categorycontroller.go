33d7f2941c33928f18c6459c2119608e538ea309
Kuroshiel <alifianfikry@gmail.com>
2024-02-12 16:48:41 +0700

    Menyiapkan Modules Categories Golang CRUD Web

---
+++ controllers/categorycontroller/categorycontroller.go
@@ -0,0 +1,21 @@
+package categorycontroller
+
+import "net/http"
+
+// Menyiapkan Modules Categories Golang CRUD Web
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
